7ad89dd11d535aef21cb6b678cf1321cf29a2116
tom110 <254449149@qq.com>
2019-06-12 14:49:29 +0800

    初步成果

--- config/config.js
+++ config/config.js
@@ -1,11 +1,33 @@
 export default {
+    singular: true,
     plugins: [
        ['umi-plugin-react', {
          // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+         antd: true
        }],
     ],
-    routes: [{
+    routes: [
+      {
         path: '/',
-        component: './helloworld',
-    }],
+        component: '../layout',
+        routes: [
+          {
+            path: '/',
+            component: './helloworld'
+          },
+          {
+            path: '/helloworld',
+            component: './helloworld'
+          },
+          {
+            path: '/dashboard',
+            routes:[
+              {path: '/dashboard/analysis',component: 'Dashboard/Analysis'},
+              {path: '/dashboard/monitor',component: 'Dashboard/Monitor'},
+              {path: '/dashboard/workplace',component: 'Dashboard/Workplace'}
+            ]
+          },
+        ]
+      }
+    ],
 };
